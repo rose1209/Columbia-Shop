803eebd6098ed183c31e4ddd88094716108dea3e
rose1209 <rose1209@gmail.com>
2020-08-19 21:14:32 +0300

    Total price & special offers

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,17 +2,26 @@ let productsList = JSON.parse(localStorage.getItem("productsList"));
 
 let cartTableBody = document.querySelector("#cart-table-body");
 
+let totalPrice = 0;
+
 for (let productTitle in productsList) {
     let productQuantity = productsList[productTitle]["productQuantity"];
     let productImage = productsList[productTitle]["productImage"];
+    let productPrice = parseFloat(productsList[productTitle]["productPrice"].slice(1));
+    let totalProductPrice = productPrice * parseInt(productQuantity);
+
+    totalPrice += totalProductPrice;
 
     cartTableBody.innerHTML += `
     <tr>
         <td>${productTitle}</td>
         <td>${productQuantity}</td>
+        <td>${productPrice}</td>
         <td><img src="${productImage}" alt="${productTitle}" class="cart-product-image"/></td>
-    </tr>`
+    </tr>`;
 }
+
+document.querySelector("#total_box_div").innerHTML = "Tota: $" + totalPrice;
 
 let clearCartBtn = document.querySelector("#clear-cart-button");
 clearCartBtn.addEventListener('click', clearCart);
